e6324b95401c69da4b5c32b759c4a5e86a80b200
jgodi96@gmail.com <jgodi96@gmail.com>
2022-06-09 14:05:16 -0700

    Creating doc in firebase working

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -13,6 +13,10 @@ const Navigation = () =>{
                 SHOP
 
               </Link>
+              <Link className="nav-link" to='/sign-in'>
+                SIGN IN
+
+              </Link>
           </div>
       
         </div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth, signInWithRedirect, signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
 
 //copy and paste from firebase documentation
 const firebaseConfig = {
@@ -18,6 +19,33 @@ const firebaseConfig = {
   provider.setCustomParameters({
       prompt:"select_account"
   });
-  
+
   // Initialize Firebase
 export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider)
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db,'users',userAuth.uid)
+
+    console.log(userDocRef)
+
+    //data
+    const userSnapshot = await getDoc(userDocRef)
+    console.log(userSnapshot.exists())
+
+    if (!userSnapshot.exists()){
+        const {displayName,email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef,{
+                displayName,email,createdAt
+            });
+        }
+
+        catch(e){
+            console.log('error creating user',e.message)
+        }
+        }
+}
