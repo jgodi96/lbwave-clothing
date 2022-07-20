fd4471fd5adf7fb9d3493cc6af352a659e814a6f
Joshua Godinez <jgodi96@gmail.com>
2022-07-20 14:45:55 -0700

    Added firebase functionality

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -46,8 +46,9 @@ export const CartContext = createContext({
     cartItems:[],
     addItemToCart:()=>{},
     addTotal:()=>{},
-    cartCount: 0,
     clearItemFromCart: () => {},
+    cartCount: 0,
+    cartTotal:0
 })
 
 
@@ -56,11 +57,20 @@ export const CartProvider = ({children}) => {
     const [isCartOpen,setIsCartOpen] = useState(false);
     const [cartItems,setCartItems] = useState([])
     const [cartCount, setCartCount] = useState(0)
+    const [cartTotal, setCartTotal] = useState(0)
 
+    //make sure it governs one responsibility
     useEffect(()=>{
         const newCartCount = cartItems.reduce((total,cartItem)=>total+cartItem.quantity,0)
         setCartCount(newCartCount)
     },[cartItems])
+
+    useEffect(()=>{
+        const newCartTotal = cartItems.reduce((total,cartItem)=>total+cartItem.quantity * cartItem.price,0)
+        setCartTotal(newCartTotal)
+    },[cartItems])
+
+
     
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems,productToAdd))
@@ -75,7 +85,7 @@ export const CartProvider = ({children}) => {
 
     
   
-    const value = {isCartOpen,setIsCartOpen, addItemToCart, cartItems,cartCount,removeItemToCart,clearItemToCart}
+    const value = {isCartOpen,setIsCartOpen, addItemToCart, cartItems,cartCount,removeItemToCart,clearItemToCart,cartTotal}
 
     return (
         <CartContext.Provider value = {value}>{children}</CartContext.Provider>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { UserProvider } from './context/user.context';
-import {ProductsProvider} from './context/products.context'
+import {CategoriesProvider} from './context/categories.context'
 import App from './App';
 import './index.styles.scss'
 import { BrowserRouter } from 'react-router-dom';
@@ -15,11 +15,11 @@ root.render(
   <React.StrictMode>
   <BrowserRouter>
   <UserProvider>
-    <ProductsProvider>
+    <CategoriesProvider>
     <CartProvider>
   <App />
   </CartProvider>
-    </ProductsProvider>
+    </CategoriesProvider>
   </UserProvider>
   </BrowserRouter>
  
