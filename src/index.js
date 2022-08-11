32bee93a3de39fde38a093d1dd6f440cba9c16ab
Joshua Godinez <jgodi96@gmail.com>
2022-08-11 11:22:44 -0700

    added stripe utils

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import App from './App';
-import './index.styles.scss'
-import { BrowserRouter } from 'react-router-dom';
-import {PersistGate} from 'redux-persist/integration/react'
-import {store,persistor} from './store/store'
+import App from "./App";
+import "./index.styles.scss";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
+import { Elements } from "@stripe/react-stripe-js";
+import {stripePromise} from './utils/stripe/stripe.utils'
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <React.StrictMode>
-  <Provider store = {store}>
-  <PersistGate loading={null} persistor = {persistor}>
-  <BrowserRouter>
-
-  <App />
-
-  </BrowserRouter>
-  </PersistGate>
-  </Provider>
- 
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+        <Elements stripe = {stripePromise}>
+          <App />
+        </Elements>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
